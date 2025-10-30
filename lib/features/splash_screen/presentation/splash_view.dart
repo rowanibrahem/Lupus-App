fd98063372cd90082149d7eecbff871d0cbebb12
Rowan Ibrahem <rowanibrahem2003@gmail.com>
2025-10-30 21:28:30 +0200

    finish splash screen

--- lib/features/splash_screen/presentation/splash_view.dart
+++ lib/features/splash_screen/presentation/splash_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lupus_app/features/splash_screen/presentation/widgets/splash_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SplashBody(),
+        ),
+        );
   }
 }
