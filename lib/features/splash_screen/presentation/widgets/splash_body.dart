08ce69303c20d93b3fe1e554c3c85677d6858685
Rowan Ibrahem <rowanibrahem2003@gmail.com>
2025-10-30 21:28:30 +0200

    finish splash screen

---
+++ lib/features/splash_screen/presentation/widgets/splash_body.dart
@@ -0,0 +1,75 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lupus_app/core/constants/app_text.dart';
+import 'package:lupus_app/core/constants/asset_images.dart';
+import 'package:lupus_app/features/on_boarding/onboarding_view.dart';
+
+class SplashBody extends StatefulWidget {
+  const SplashBody({super.key});
+
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const OnBoardingView()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ScaleTransition(
+            scale: _animation,
+            child: FadeTransition(
+              opacity: _animation,
+              child: Center(
+                child: Image.asset(
+                AssetImages.logo,
+                  // width: 150,
+                  // height: 150,
+                  ),
+              ),
+             ),
+          ),
+          Text(
+          AppText.appName,
+          ),
+          ],
+          ),
+    );
+  }
+}
