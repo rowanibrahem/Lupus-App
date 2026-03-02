c774362a7c7b68ddfbd53aa8aea0a6ce36a777b8
Rowan Ibrahem <rowanibrahem2003@gmail.com>
2026-03-02 23:26:19 +0200

    first intialize

---
+++ lib/features/auth/data/modals/user_type_modal.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class UserTypeModal {
+  final String text;
+  final String icon;
+  final VoidCallback func;
+  
+  UserTypeModal({
+    required this.text,
+    required this.icon,
+    required this.func,
+  });
+}
