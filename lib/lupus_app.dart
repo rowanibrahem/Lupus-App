4e877f5da7f9763f449c0dacee42b183c5611f60
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2026-04-18 12:16:32 +0200

    Feature(signup): build Pharmacy registration UI
    UI: assets/screenshots/PharmacySignupView.png

--- lib/lupus_app.dart
+++ lib/lupus_app.dart
@@ -4,7 +4,7 @@ import 'package:lupus_app/core/services/app_router.dart';
 import 'package:lupus_app/core/services/navigation_service.dart';
 import 'package:lupus_app/core/theme/app_theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:lupus_app/features/auth/presentation/views/charity_signup/charity_signup_view.dart';
+import 'package:lupus_app/features/auth/presentation/views/pharmacy_signup/pharmacy_signup_view.dart';
 
 class LupusApp extends StatelessWidget {
   const LupusApp({super.key});
@@ -30,7 +30,7 @@ class LupusApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         onGenerateRoute: AppRouter.generateRoute,
         navigatorKey: NavigationService.navigatorKey,
-        home: CharitySignupView(),
+        home: PharmacySignupView(),
       ),
     );
   }
